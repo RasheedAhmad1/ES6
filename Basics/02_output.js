5c5e9f47f3c490678790a015dffd63f86ecc88f4
Rasheed_Ahmad <rasheedahmad22333@gmail.com>
2022-07-01 22:56:40 +0500

    JS code is separated from their respective HTML file

---
+++ Basics/02_output.js
@@ -0,0 +1,11 @@
+// --- Using innerHTML ---
+document.getElementById("demo").innerHTML = "I am loaded";
+
+// --- Using document.write() ---
+document.write(5+6);
+
+// --- Using window.alert() ---
+window.alert('Hello');
+
+// --- Using console.log() ---
+console.log(1+2);
