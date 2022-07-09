5a46d27f30ece80908463db28b2b136dd2bbcb69
Rasheed_Ahmad <rasheedahmad22333@gmail.com>
2022-07-09 12:11:38 +0500

    Utilities file added to the modules, to discuss 'imports' and 'exports' in JavaScript

---
+++ Intermediate/Modules/utils.js
@@ -0,0 +1,12 @@
+// Define a constant variable
+// export const name = 'Rasheed Jan';
+
+// Define a function to 'Add' two numbers
+export function addNumbers(a, b) {
+    return a + b;
+}
+
+// Define a function to 'Multiply' two numbers
+function multiplyNumbers(a, b) {
+    return a * b;
+}
