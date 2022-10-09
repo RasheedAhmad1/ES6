6271a68921b4d548994fb2735ef02d7554e0a2d2
Rasheed_Ahmad <rasheedahmad22333@gmail.com>
2022-10-09 07:46:31 +0500

    Spread Operator added to the file

--- Intermediate/spread.js
+++ Intermediate/spread.js
@@ -2,6 +2,9 @@
 // to be expanded where zero or more arguments (for ftn call) or 
 // elements (for array lietrals) are expected.
 
+// const newArray = [...oldArray, 1, 2];
+// const newObject = {...oldObject, newProp:5}
+
 function sum(x, y, z) {
     return x+y+z;
 }
@@ -18,10 +21,15 @@ const [addition, multiplication] = addAndMultiply(2, 3);
 console.log(`Addition is: ${addition} and Multiplication is: ${multiplication}`);
 
 
-// Rest Operator allows an iterable to pack him
-// e.g:
-const vowels = ['A', 'E', 'I', 'O', 'U']
+// Rest Operator is used to merge a list of ftn arguments into an array
+// e.g1:
+const vowels = ['A', 'E', 'I', 'O', 'U'];
 const [aa, bb, ...rest] = vowels;
 console.log(aa);
 console.log(bb);
 console.log(rest);
+
+// e.g2:
+function sortArgs(...args) {
+    return args.sort();
+}
