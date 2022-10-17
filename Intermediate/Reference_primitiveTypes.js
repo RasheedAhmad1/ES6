b887cfafd372f4716a46360549e2c03caf9ca34a
Rasheed_Ahmad <rasheedahmad22333@gmail.com>
2022-10-17 21:44:20 +0500

    Primitive and Reference Types JS file is added to the directory

---
+++ Intermediate/Reference_primitiveTypes.js
@@ -0,0 +1,15 @@
+// Reference Types
+num1 = 1;
+num2 = num1;
+console.log(num1); // 1
+console.log(num2); // 1
+
+// Primitive Types
+const Person1 = {
+    name: 'Adam'
+}
+
+const Person2 = Person1;
+
+console.log(Person1);
+console.log(Person2);
