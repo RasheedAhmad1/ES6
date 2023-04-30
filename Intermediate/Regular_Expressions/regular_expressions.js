16113a6e60f5cb05c7f73dece3342acc78a131f5
Rasheed_Ahmad <rasheedahmad22333@gmail.com>
2023-04-30 21:10:57 +0500

    Some useful Regular Expressions added to the file

--- Intermediate/Regular_Expressions/regular_expressions.js
+++ Intermediate/Regular_Expressions/regular_expressions.js
@@ -1,2 +1,5 @@
 // Simple regular expression
  
+const Color_Hex_code = `#[a-zA-Z0-9]{6} or #[\w]{6}`;
+const Contact_no = `^(\+92)?[\d]{7}$`; // 92 is optional here
+const URL_pattern = `/www\.[\w]+\.(com|co|org|in)$/g`; 
