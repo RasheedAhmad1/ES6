4c669ea187f798a298eeedab01410977d800de5c
Rasheed_Ahmad <rasheedahmad22333@gmail.com>
2023-04-30 21:10:18 +0500

    Blob objects defined

--- Async_JavaScript/main.js
+++ Async_JavaScript/main.js
@@ -1,4 +1,4 @@
-// Fetching data with fetch
+/* ==== Fetching data with fetch API ==== */
 // fetch('../assets/DOM.png').
 // then( (response) => {
 //     return response.blob();
@@ -10,7 +10,7 @@
 // });
 // catchImage()
 
-// The above task with async and await
+/* ==== The above task with async and await ==== */
 async function catchImage () {
     const response = await fetch('../assets/DOM.png');
     const blob = await response.blob();
@@ -25,3 +25,8 @@ catchImage()
         console.log('Our own error!');
         console.log(error);
     });
+
+// The Blob object represents a blob, which is a file-like object of immutable,
+// raw data; they can be read as text or binary data, or converted into a 
+// ReadableStream so its methods can be used for processing the data. 
+// Blobs can represent data that isn't necessarily in a JavaScript-native format.
